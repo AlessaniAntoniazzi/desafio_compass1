c5443a357983a19a00c7b7cdbbe25a4122a04ae2
Alessani Antoniazzi <alessaniantoniazzi@gmail.com>
2023-11-08 23:02:23 -0300

    adding eslint, package-lock.json, package.json, pet and tutos models, and a little bit of routes

---
+++ src/models/petModel.ts
@@ -0,0 +1,8 @@
+interface Pet {
+    id: number;
+    name: string;
+    species: string;
+    carry: string;
+    weight: number;
+    date_of_birth:string;
+}
---
+++ src/models/tutorModel.ts
@@ -0,0 +1,10 @@
+interface Tutors {
+    id: number;
+    name: string;
+    phone: number;
+    email: string;
+    date_of_birth: string;
+    zipCode: string;
+    pets: Pet[];
+
+}
